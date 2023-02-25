087674ff3dd668e37eb74540ff8d4a6a3c12f7e5
smriti32 <maharjanbiplope16@gmail.com>
2023-02-25 17:41:52 +0545

    Fantech

--- src/main/java/com/example/bakeryShop/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bakeryShop.repository;
-
-import com.example.bakeryShop.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByCategoryName(String name);
-}
